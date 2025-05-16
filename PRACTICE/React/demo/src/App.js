ac70766290d197efd7b6e7cfd344fbd686b23c46
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-05-16 05:09:13 -0700

    file name

--- PRACTICE/React/demo/src/App.js
+++ PRACTICE/React/demo/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Classcompo from "./Compent/Classcompo";
+// import Funcompo from "./Compent/Funcompo";
+// import Hello from "./Jsx/Hello";
+// import Hello2 from "./Jsx/Hello2";
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>hello this App data</h1>
+            {/* <h1>hello this h1</h1> */}
+
+            {/* Compoennt */}
+            {/* <Classcompo /> */}
+            {/* <Funcompo /> */}
+        
+            {/* jsx compo */}
+            {/* <Hello /> */}
+            {/* <Hello2 /> */}
+
+
+            {/* CSS Compoennt */}
+            
+        </div>
+    )
 }
 
 export default App;
---
+++ PRACTICE/React/demo/src/Component/Classcompo.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+ 
+
+export class Classcompo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This iS A Div</h1>
+      </div>
+    )
+  }
+}
+
+ 
