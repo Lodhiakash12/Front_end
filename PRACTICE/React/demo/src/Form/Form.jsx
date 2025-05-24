050b7d006b192b0a033137822c04498cfe35bb3a
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-05-24 09:38:47 -0700

    file name

--- PRACTICE/React/demo/src/Form/Form.jsx
+++ PRACTICE/React/demo/src/Form/Form.jsx
@@ -6,16 +6,20 @@ export default function Form() {
 
   const [data, setdata] = useState({
 
-    username: "",
-    email: ""
+    name: "",
+    email: "",
+    phone:"",
+    password:""
+
+  
 
   })
 
 
-  const getchange= (e) =>{
+  const getchange = (e) => {
     setdata({
       ...data,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
@@ -30,29 +34,29 @@ export default function Form() {
       <input type="text" placeholder="enter Email" value={data.email}  onChange={(E) => setdata({...data,email:E.target.value})} /> */}
 
 
-       <form action="">
-                            <div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleFormControlInput2" className="form-label">Enter your Name</label>
-                                    <input type="text" name='name' value={data.name} onChange={getchange} className="form-control" id="exampleFormControlInput2" placeholder="Enter your name" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                                    <input type="email" name='email' value={data.email} onChange={getchange} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleFormControlInput3" className="form-label">Enter your number</label>
-                                    <input type="tel" name='phone' value={data.phone} onChange={getchange} className="form-control" id="exampleFormControlInput3" placeholder="Enter your number" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleFormControlInput4" className="form-label">Enter your Password</label>
-                                    <input type="password" name='password' value={data.password} onChange={getchange} className="form-control" id="exampleFormControlInput4" placeholder="Enter your Password" />
-                                </div>
-                                <div>
-                                    <input type="submit" />
-                                </div>MN
-                            </div>
-                        </form>
+      <form action="">
+        <div className='container'>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput2" className="form-label">Enter your Name</label>
+            <input type="text" name='name' value={data.name} onChange={getchange} className="form-control" id="exampleFormControlInput2" placeholder="Enter your name" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+            <input type="email" name='email' value={data.email} onChange={getchange} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput3" className="form-label">Enter your number</label>
+            <input type="tel" name='phone' value={data.phone} onChange={getchange} className="form-control" id="exampleFormControlInput3" placeholder="Enter your number" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput4" className="form-label">Enter your Password</label>
+            <input type="password" name='password' value={data.password} onChange={getchange} className="form-control" id="exampleFormControlInput4" placeholder="Enter your Password" />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </div>
+      </form>
 
 
     </div>
