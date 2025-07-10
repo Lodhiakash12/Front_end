a5edd0d3b9a94c954342266878839a05011469bd
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-07-10 04:13:18 -0700

    file name

--- PRACTICE/React/redux-crude/src/App.jsx
+++ PRACTICE/React/redux-crude/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Read from "./components/Read"
 import Create from "./components/Create"
 import Update from "./components/Update"
+import Anab from "./components/ADMIN/common/Anab"
 
 function App() {
 
@@ -18,6 +19,11 @@ function App() {
             <Route path='/' element={<Read />} />
             <Route path='/add' element={<Create />} />
             <Route path='/update' element={<Update />} />
+
+
+            {/* admin */}
+            <Route path='/nav' element={<Anab />} />
+
           </Routes>
 
         </div>
