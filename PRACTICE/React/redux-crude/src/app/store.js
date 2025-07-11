599b03b4edf9680db09328b5b16b32f82f2d6ae7
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-07-11 08:45:47 -0700

    file name

--- PRACTICE/React/redux-crude/src/app/store.js
+++ PRACTICE/React/redux-crude/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../Slicedata/userSlice";
-import { adminSlice } from "../Slicedata/adminSlice";
+import  adminSlice  from "../Slicedata/adminSlice";
 
 export const store = configureStore({
     reducer: {
